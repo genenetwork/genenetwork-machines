dc3d27b29f7c50a55baf8ee259e5ca4854f62a68
Arun Isaac <arunisaac@systemreboot.net>
2022-10-12 01:04:58 +0530

    Add virtuoso container for tux01 production.
    * virtuoso.scm, virtuoso-deploy.sh: New files.
    * README.org (Virtuoso container for tux01 production): New section.

---
+++ virtuoso-deploy.sh
@@ -0,0 +1,31 @@
+#! /bin/sh -e
+
+# genenetwork-machines --- Guix configuration for genenetwork machines
+# Copyright © 2022 Arun Isaac <arunisaac@systemreboot.net>
+#
+# This file is part of genenetwork-machines.
+#
+# genenetwork-machines is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# genenetwork-machines is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with genenetwork-machines.  If not, see
+# <https://www.gnu.org/licenses/>.
+
+# Build and install virtuoso container on tux01.
+
+container_script=$(guix system container \
+                        --network \
+                        --verbosity=3 \
+                        --share=/var/guix-containers/virtuoso/var/lib/virtuoso=/var/lib/virtuoso \
+                        virtuoso.scm)
+
+echo $container_script
+sudo ln --force --symbolic $container_script /usr/local/bin/virtuoso-container
---
+++ virtuoso.scm
@@ -0,0 +1,37 @@
+;;; genenetwork-machines --- Guix configuration for genenetwork machines
+;;; Copyright © 2022 Arun Isaac <arunisaac@systemreboot.net>
+;;;
+;;; This file is part of genenetwork-machines.
+;;;
+;;; genenetwork-machines is free software: you can redistribute it
+;;; and/or modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation, either version 3 of
+;;; the License, or (at your option) any later version.
+;;;
+;;; genenetwork-machines is distributed in the hope that it will be
+;;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;;; See the GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with genenetwork-machines.  If not, see
+;;; <https://www.gnu.org/licenses/>.
+
+(use-modules (gnu)
+             (gn services databases))
+
+(operating-system
+  (host-name "virtuoso")
+  (timezone "UTC")
+  (locale "en_US.utf8")
+  (bootloader (bootloader-configuration
+               (bootloader grub-bootloader)
+               (targets (list "/dev/sdX"))))
+  (file-systems %base-file-systems)
+  (users %base-user-accounts)
+  (packages %base-packages)
+  (services (cons (service virtuoso-service-type
+                           (virtuoso-configuration
+                            (server-port 8891)
+                            (http-server-port 8892)))
+                  %base-services)))
